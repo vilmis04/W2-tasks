c926d7af85e363be1cdc691c0beffcfa95c0d0e3
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-02-14 22:28:08 +0200

    tasks 1.2 & 1.3 completed

--- 1_1_task.php
+++ 1_1_task.php
@@ -39,37 +39,80 @@ $inventory = [
 $shoppingList = transformUserInputToArray($argv[1]);
 printBill($inventory, $shoppingList);
 
-// var_dump($shoppingList);
-
-
 // functions
 
 function transformUserInputToArray (string $input): array
-    {
-        $inputArray = explode(' ', $input);
-        $shoppingList = [];
-        foreach ($inputArray as $row) {
-            $item = explode(':', $row);
-            $shoppingList[] = $item;
-        }
-        return $shoppingList;
+{
+    $inputArray = explode(' ', $input);
+    $shoppingList = [];
+    foreach ($inputArray as $row) {
+        $item = explode(':', $row);
+        $shoppingList[] = $item;
     }
+    return $shoppingList;
+}
 
 
 function printBill(array $stock, array $list): void
-    {   
+{   
+    $itemsToPrint = getPrintableList($stock, $list);
+    $isInStock = $itemsToPrint[0]['inStock'];
+    $listToPrint = $itemsToPrint[1];
+    if ($isInStock) {
         $total = 0;
-        foreach($list as $row) {
-            $item = $row[0];
-            $quantity = $row[1];
-            $subtotal = $stock[$item]['price'] * $quantity;
-            $total += $subtotal;
-            
-            echo '*****' . PHP_EOL;
-            echo $item . PHP_EOL;
-            echo $stock[$item]['price'] . ' * ' . $quantity . ' = ' . $subtotal;
-            echo PHP_EOL;
+        foreach ($listToPrint as $item) {
+            printItem($item['item'], $item['price'], $item['quantity']);
+            $total += $item['price']*$item['quantity'];
         }
         echo '*****' . PHP_EOL;
         echo 'Total: ' . $total . PHP_EOL;
+    } else {
+        echo 'Error!' . PHP_EOL;
+        foreach ($listToPrint as $item) {
+            echo $item.PHP_EOL;
+        }
     }
+}
+
+function printItem (string $item, float $price, int $quantity): void
+{
+    echo '*****' . PHP_EOL;
+    echo $item . PHP_EOL;
+    echo $price . ' * ' . $quantity . ' = ' . $price*$quantity;
+    echo PHP_EOL;   
+}
+
+function getPrintableList (array $stock, array $list): array
+{
+    $existingItems = [];
+    $lowStockItems = [];
+    $result = [];
+    foreach($list as $row) {
+        $item = $row[0];
+        $quantity = intval($row[1]);
+
+        if (array_key_exists($item, $stock)) {
+            $remainingStock = $stock[$item]['count'];
+            if ($quantity <= $remainingStock) {
+                $existingItems[] = [
+                    'item' => $item,
+                    'price' => $stock[$item]['price'],
+                    'quantity' => $quantity
+                ];
+            } else {
+                $lowStockItems[] = 'We only have '.$remainingStock.' '.$item.', '.'you asked for '.$quantity.' '.$item;
+            }
+        } else {
+            $lowStockItems[] = 'Item '.$item.' not found in the shop!';
+        }
+    }
+    
+    if (count($lowStockItems)>0) {
+        $result[] = ['inStock' => false];
+        $result[] = $lowStockItems;
+    } else {
+        $result[] = ['inStock' => true];
+        $result[] = $existingItems;
+    }
+    return $result;
+}
